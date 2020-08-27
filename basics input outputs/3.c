b523fabb2eb2e2aa274ab1232ce7d8ae895f2d89
Rohan-shakya <rohanshakya254@gmail.com>
2020-08-27 22:14:53 +0545

    added 3 program

---
+++ basics input outputs/3.c
@@ -0,0 +1,10 @@
+// WAP ro enter full name and print it on screen.
+#include<stdio.h>
+void main()
+{
+	char name[100];
+	puts("Enter your full name");
+	gets(name);
+	puts("Your Name is:");
+	puts(name);
+}
