6af23d56089b8702d9428fdab0c7e9274f95d261
Rohan-shakya <rohanshakya254@gmail.com>
2020-08-27 22:09:49 +0545

    second program

---
+++ basics input outputs/2-sqaure.c
@@ -0,0 +1,11 @@
+// WAP to find square of a number
+#include<stdio.h>
+void main()
+{
+	int a, b;
+	printf("\nEnter a number");
+	scanf("%d", &a);
+	b = a * a;
+	printf("Square = %d", b);
+	getch();
+}
