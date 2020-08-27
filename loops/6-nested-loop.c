adc30eb0a75cb9e5b521734804c6692bd23c96dd
Rohan-shakya <rohanshakya254@gmail.com>
2020-08-28 00:00:26 +0545

    added nested loop problem

---
+++ loops/6-nested-loop.c
@@ -0,0 +1,22 @@
+// WAP to display the multiplication table of nth terms
+// 1 2 3 ...... N
+// 2 4 6 .....2N
+// 3 6 9 .... 3N
+// . . .      .
+// . . .      .
+// N 2N 3N ...NN
+
+#include<stdio.h>
+void main()
+{
+	int i, j , n;
+	printf("\nEnter the number of term:\n");
+	scanf("%d", &n);
+	for(i = 1; i <= n; i++){
+		for(j = 1; j <= n; j++){
+			printf("%5d", i*j);
+		}
+		printf("\n");
+	}
+	getch();
+}
