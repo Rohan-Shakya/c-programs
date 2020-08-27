91b5868d65812750300442f10ef119dd769872d0
Rohan-shakya <rohanshakya254@gmail.com>
2020-08-27 22:02:16 +0545

    first program sum

---
+++ basics input outputs/1-sum.c
@@ -0,0 +1,13 @@
+// WAP to find a sum of two input numbers
+#include<stdio.h>
+void main()
+{
+	int a, b, c;
+	printf("\nEnter first number");
+	scanf("%d", &a);
+	printf("\nEnter second number");
+	scanf("%d", &b);
+	c = a + b;
+	printf("Sum is %d", c);
+	getch();
+}
