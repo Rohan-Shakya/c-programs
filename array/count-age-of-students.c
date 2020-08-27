5b71a4b6c324e59950cd5e66b8e85e0c22e6be8b
Rohan-shakya <rohanshakya254@gmail.com>
2020-08-28 01:02:22 +0545

    added array students count

---
+++ array/count-age-of-students.c
@@ -0,0 +1,17 @@
+// WAP to input the age of 2 students and count the number of students having age between 20 to 25.
+#include<stdio.h>
+void main()
+{
+	int i , age[10], c = 0;
+	for(i = 0; i < 20; i++){
+		printf("Enter age of students:");
+		scanf("%d", age[i]);
+	}
+	for(i = 0; i < 20; i++){
+		if(age[i] >= 20 && age[i]<= 25){
+			c++;
+		}
+	}
+	printf("total number of studnet having age bwtweon 20 -25 is: %d", c);
+	getch();
+}
