4f25b9fe0702efca9c1448aa1c67d409f61b057a
Rohan-shakya <rohanshakya254@gmail.com>
2020-08-28 09:44:04 +0545

    added recursion problem-1

---
+++ recursion/factorial.c
@@ -0,0 +1,21 @@
+// WAP to calculate factorial using recursion fucntion
+#include<stdio.h>
+int fact(int);
+void main()
+{
+	int a, f;
+	printf("\nEnter a number:\n");
+	scanf("%d", &n);
+	f = fact(n);
+	printf("\nfactorial of %d is %d", n, f);
+}
+int fact(int n)
+{
+	if(n <=1)
+	{
+		return(1);
+	}
+	else{
+		return(n*fact(n-1));
+	}
+}
